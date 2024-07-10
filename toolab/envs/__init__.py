f4d12614319b9940200fc8216567583881fe7787
Vater Hu <92420346+WhatMelonGua@users.noreply.github.com>
2024-07-11 00:38:52 +0800

    Add files via upload

---
+++ toolab/envs/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# # # # # # # # # # # # 
+"""
+ ╭───────────────────────────────────────╮  
+ │ __init__.py.py   2024/7/11-0:16
+ ╰───────────────────────────────────────╯ 
+ │ Description:
+    Store Py-Site-Env, set by custom
+"""  # [By: HuYw]
+
+# region |- Import -|
+
+# endregion
