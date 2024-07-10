a600261044e7908489bf2a6070b3d016bac67f3d
Hu <1107818699@qq.com>
2024-07-11 02:25:42 +0800

    time & logs

---
+++ toolab/logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# # # # # # # # # # # # 
+"""
+ ╭───────────────────────────────────────╮  
+ │ logger.py   2024/7/11-2:22
+ ╰───────────────────────────────────────╯ 
+ │ Description:
+    Logger info/ just solve for problem with osError: disk quote!
+"""  # [By: HuYw]
+
+# region |- Import -|
+import logging
+# endregion
