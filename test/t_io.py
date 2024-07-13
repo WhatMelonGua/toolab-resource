d0eaae4b53cdf982f1ffef260be24f1ad7e07320
Hu <1107818699@qq.com>
2024-07-13 18:08:41 +0800

    IO

--- test/t_io.py
+++ test/t_io.py
@@ -6,7 +6,8 @@
  │ t_io.py   2024/7/11-20:34
  ╰───────────────────────────────────────╯ 
  │ Description:
-    
+    主要测试 内存不足的Promise操作
+    模拟Linux下的内存不足，教程源自：https://www.jianshu.com/p/b6fb59508c7b
 """  # [By: HuYw]
 
 # region |- Import -|
@@ -17,3 +18,5 @@ print(toolab.io.getRunDir())
 
 
 pio = toolab.io.PromiseIO()
+
+# pio.applyBlock('test', 4)
